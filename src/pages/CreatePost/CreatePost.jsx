a6594026d30c93033e5570889af1229ceed3be54
gabriel degobi <gwillians98@gmail.com>
2025-08-02 16:28:42 -0300

    consertando bugs

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -104,6 +104,7 @@ const CreatePost = () => {
         {!response.loading && <button className='btn'>Cadastrar</button>}
         {response.loading && <button className='btn'>Cadastrar...</button>}
         {response.error && <p className='error'>{response.error}</p>}
+        {formError && <p className='error'>{formError}</p>}
       </form>
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
 import styles from "./Home.module.css"
 
+//Hooks
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+
+
 const Home = () => {
+  const [query, setQuery] = useState('')
+  const [posts, setPosts] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Home</div>
+    <div className={styles.home}>
+      <h1>Vejas os nossos posts mais recentes</h1>
+      <form className={styles.search_form} onSubmit={handleSubmit}>
+        <input type="text" placeholder="Ou busque por tags..." onChange={(e) => setQuery(e.target.value)} />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h1>posts...</h1>
+        {posts && posts.length === 0 &&
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn btn-dark">Criar primeiro post</Link>
+          </div>
+        }
+      </div>
+    </div>
   )
 }
 
