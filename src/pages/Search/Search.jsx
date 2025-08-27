632d30a7ead910f1c278c832784edf93982d2494
gabriel degobi <gwillians98@gmail.com>
2025-08-27 17:16:44 -0300

    consertando bugs

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -3,13 +3,24 @@ import React from 'react'
 
 import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 import { useQuery } from '../../hooks/useQuery'
+
+//COMPONENTS
+import PostDetail from "../../components/PostsDetail/PostDetail"
+
 const Search = () => {
   const query = useQuery()
   const search = query.get("q")
+
+  const { documents: posts } = useFetchDocuments('posts')
+
   return (
     <div>
       <h2>Search</h2>
-      <p>{search}</p>
+      <div>
+        {posts && posts.map((post) => (
+          <PostDetail key={post.id} post={post}/>
+        ))}
+      </div>
     </div>
   )
 }
